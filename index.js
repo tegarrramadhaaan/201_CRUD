e733c1a3ddffea800b143e8b09d411e1be6d4655
tegarrramadhaaan <anakrama2000@gmail.com>
2025-10-21 10:50:26 +0700

    add MySQL database connection setup

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ app.get('/',(req,res) => {
     res.send('Hello World');
 });
 
+app.listen(PORT, () => {
+    console.log('server is running on port ${PORT}');
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'biodata',
+  port : 3308 ,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:' + err.stack);
+    return;
+  }
+    console.log('Connection Succuessfuly!');
+});
+
